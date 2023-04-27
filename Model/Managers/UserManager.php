167d8fee2624ec11a0f6b4f5315101988f2c52f3
Fourmaux Gregory <2201504@afpa.fr>
2023-04-27 08:34:12 +0200

    ajout du login non chiffré

--- Model/Managers/UserManager.php
+++ Model/Managers/UserManager.php
@@ -27,8 +27,23 @@ class UserManager{
         return $newUser;
     }
 
-    public static function connectUser()
+    public static function connectUser($pseudo, $password)
     {
-        
+        $pdo = dbconnect();
+        $sql = "SELECT * FROM users WHERE pseudo= :pseudo AND password=:password";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam('pseudo', $pseudo);
+        $stmt->bindParam(':password', $password);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+        $result = $stmt->fetch();
+        return $result;
+        /*if($result) {
+            $passwordRegistered = $result->getPassword();
+            if($passwordRegistered === $password){
+                return $result;
+            }
+        }else{
+            return false;
+        }*/
     }
 }
---
+++ login.php
@@ -0,0 +1,12 @@
+<?php 
+
+require_once 'Model/Managers/UserManager.php';
+
+if(isset($_POST) && !empty($_POST)){
+    $pseudo = $_POST['pseudo'];
+    $password = $_POST['password'];
+    $user = UserManager::connectUser($pseudo, $password);
+    var_dump($user);
+}
+
+require_once 'Views/loginView.php';
